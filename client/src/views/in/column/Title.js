d61f95ad76b3384ca21de828cd006d6200d2f46c
cynthialin1228 <cynthialin1228@gmail.com>
2023-05-16 18:34:26 +0800

    change Title prettier

--- client/src/views/in/column/Title.js
+++ client/src/views/in/column/Title.js
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+// import background from '../../../assets/images/background.png'
 
 const Title = ({ data }) => {
   const name = data.name
   const exp = data.experience
+  // const file = data.imgSrc
   const hashtags = data.hashtags.map((hashtag) => {
     return (
       <nobr key={hashtag}>
@@ -15,12 +17,16 @@ const Title = ({ data }) => {
   })
   return (
     <header>
-      <div className="row banner">
+      <div className="banner">
         <div className="banner-text">
+          {/* <hr/> */}
           <h1 className="responsive-headline">{name}</h1>
-          {exp}
+          {/* <hr/> */}
+          <h1></h1>
+          <div className="exp-text">{exp}</div>
+          {/* <hr /> */}
+          <h1></h1>
           {hashtags}
-          <hr />
         </div>
       </div>
     </header>
