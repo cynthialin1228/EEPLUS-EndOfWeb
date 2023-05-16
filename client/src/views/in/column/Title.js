bfaf3c6a2d2675eec97e3b3feac34c0d66c11f89
Cynthia <cynthialin1228@gmail.com>
2023-05-16 19:30:41 +0800

    remove useless comments

--- client/src/views/in/column/Title.js
+++ client/src/views/in/column/Title.js
@@ -1,11 +1,9 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-// import background from '../../../assets/images/background.png'
 
 const Title = ({ data }) => {
   const name = data.name
   const exp = data.experience
-  // const file = data.imgSrc
   const hashtags = data.hashtags.map((hashtag) => {
     return (
       <nobr key={hashtag}>
@@ -19,12 +17,9 @@ const Title = ({ data }) => {
     <header>
       <div className="banner">
         <div className="banner-text">
-          {/* <hr/> */}
           <h1 className="responsive-headline">{name}</h1>
-          {/* <hr/> */}
           <h1></h1>
           <div className="exp-text">{exp}</div>
-          {/* <hr /> */}
           <h1></h1>
           {hashtags}
         </div>
