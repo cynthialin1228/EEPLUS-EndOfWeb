fe01de85bc2a1ed2122a1eafc1a2f523ff233937
cynthialin1228 <cynthialin1228@gmail.com>
2023-12-03 22:03:18 +0800

    only shows defaultLayout when no advisProf exists

--- client/src/layout/DefaultLayout.js
+++ client/src/layout/DefaultLayout.js
@@ -4,47 +4,51 @@ import { useSelector } from 'react-redux'
 import { selectLogin } from '../slices/loginSlice'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
 import { CModal, CModalHeader, CModalTitle, CModalBody, CModalFooter, CButton } from '@coreui/react'
+import axios from 'axios'
 
 const DefaultLayout = () => {
   const pathname = useLocation().pathname.split('/')[1]
   const { isLogin } = useSelector(selectLogin)
-  const noModal = ['forget', 'register_entry', 'reset_password', 'change_password']
+  const noModal = [
+    'edit_profile',
+    'profile',
+    'forget',
+    'register_entry',
+    'reset_password',
+    'change_password',
+  ]
   const [isModal, setIsModal] = useState(false)
   const { studentID } = useSelector(selectLogin)
   useEffect(() => {
-    setIsModal(!noModal.includes(pathname) && isLogin)
-  }, [isLogin])
-  useEffect(() => {
-    if (pathname == 'profile' && isLogin) {
+    if (isLogin) {
+      axios
+        .get('api/profile')
+        .then((res) => {
+          if (res.data.advisingProfessor.length) {
+            setIsModal(false)
+          } else {
+            setIsModal(!noModal.includes(pathname))
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    } else {
       setIsModal(false)
     }
-  }, [pathname])
+  }, [isLogin])
   return (
     <>
-     <CModal size="l" visible={isModal} onDismiss={() => setIsModal(false)} alignment="center">
+      <CModal size="l" visible={isModal} onDismiss={() => setIsModal(false)} alignment="center">
         <CModalHeader onDismiss={() => setIsModal(false)}>
           <CModalTitle>請大家去新增專題教授</CModalTitle>
         </CModalHeader>
         <CModalBody>
-          EE+推出新功能，讓大家可以在上面登錄自己跟過的專題教授啦~ 請點擊{' '}
-          <a href={`/profile/${studentID}`}>此連結 </a> 新增您跟過的專題教授吧！
+          EE+推出新功能，讓大家可以在上面登錄自己跟過的專題教授啦~
+          請點擊下方按鈕新增您跟過的專題教授吧！
         </CModalBody>
         <CModalFooter>
-          <CButton
-            color="warning"
-            onClick={() => {
-              setIsModal(false)
-            }}
-          >
-            已經填寫
-          </CButton>
-          <CButton
-            href={`/profile/${studentID}`}
-            color="primary"
-            onClick={() => {
-              setIsModal(false)
-            }}
-          >
+          <CButton href={`/edit_profile`} color="primary">
             立即填寫
           </CButton>
         </CModalFooter>
